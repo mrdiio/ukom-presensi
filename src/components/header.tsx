71153588851298d6ffbb95fdfb66b4bcf4ccbe05
Dio <dionatsir@gmail.com>
2024-12-01 16:59:48 +0700

    Enhance dashboard layout with session redirect and update header component for logout functionality

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,4 @@
-'use client';
+'use client'
 
 import {
   DropdownMenu,
@@ -7,32 +7,35 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
-import { ChevronDown, UserCircle } from 'lucide-react';
-import { useClock } from '@/hooks/useClock';
+} from '@/components/ui/dropdown-menu'
+import { ChevronDown, UserCircle } from 'lucide-react'
+import { useClock } from '@/hooks/useClock'
+import { signOut } from 'next-auth/react'
 
 export const Header = ({ userName }: { userName: string }) => {
-  const currentTime = useClock();
+  const currentTime = useClock()
 
   return (
-    <nav className='border-b h-20 bg-primary text-primary-foreground px-3'>
-      <div className='flex items-center justify-between h-full max-w-7xl mx-auto'>
-        <h1 className='text-2xl font-bold'>Presensi Dosen</h1>
-        <div className='flex gap-3'>
+    <nav className="border-b h-20 bg-primary text-primary-foreground px-3">
+      <div className="flex items-center justify-between h-full max-w-7xl mx-auto">
+        <h1 className="text-2xl font-bold">Presensi Dosen</h1>
+        <div className="flex gap-3">
           <p>{currentTime}</p>
           <DropdownMenu>
-            <DropdownMenuTrigger className='flex items-center gap-1'>
-              <UserCircle className='w-6 h-6' /> {userName || 'Unknown'}{' '}
-              <ChevronDown className='w-4 h-4' />
+            <DropdownMenuTrigger className="flex items-center gap-1">
+              <UserCircle className="w-6 h-6" /> {userName || 'Unknown'}{' '}
+              <ChevronDown className="w-4 h-4" />
             </DropdownMenuTrigger>
             <DropdownMenuContent>
               <DropdownMenuLabel>My Account</DropdownMenuLabel>
               <DropdownMenuSeparator />
-              <DropdownMenuItem>Logout</DropdownMenuItem>
+              <DropdownMenuItem onClick={() => signOut()}>
+                Logout
+              </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
         </div>
       </div>
     </nav>
-  );
-};
+  )
+}
