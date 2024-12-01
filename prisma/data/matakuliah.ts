652179fc9cc9db43b088c1f8e83f813a80b91cfb
Dio <dionatsir@gmail.com>
2024-12-01 12:54:03 +0700

    feat: add initial data for Dosen and MataKuliah, and implement seeding logic

---
+++ prisma/data/matakuliah.ts
@@ -0,0 +1,62 @@
+export const MataKuliahs = [
+  {
+    kode: 'MK001',
+    nama: 'Algoritma dan Pemrograman',
+    sks: 3,
+    jenis: 'TEORI',
+  },
+  {
+    kode: 'MK002',
+    nama: 'Pemrograman Dasar Praktikum',
+    sks: 2,
+    jenis: 'PRAKTIKUM',
+  },
+  {
+    kode: 'MK003',
+    nama: 'Matematika Diskrit',
+    sks: 4,
+    jenis: 'TEORI',
+  },
+  {
+    kode: 'MK004',
+    nama: 'Jaringan Komputer Praktikum',
+    sks: 2,
+    jenis: 'PRAKTIKUM',
+  },
+  {
+    kode: 'MK005',
+    nama: 'Sistem Operasi',
+    sks: 3,
+    jenis: 'TEORI',
+  },
+  {
+    kode: 'MK006',
+    nama: 'Pengantar Basis Data',
+    sks: 3,
+    jenis: 'TEORI',
+  },
+  {
+    kode: 'MK007',
+    nama: 'Rekayasa Perangkat Lunak',
+    sks: 4,
+    jenis: 'TEORI',
+  },
+  {
+    kode: 'MK008',
+    nama: 'Praktikum Basis Data',
+    sks: 2,
+    jenis: 'PRAKTIKUM',
+  },
+  {
+    kode: 'MK009',
+    nama: 'Kecerdasan Buatan',
+    sks: 3,
+    jenis: 'TEORI',
+  },
+  {
+    kode: 'MK010',
+    nama: 'Praktikum Sistem Digital',
+    sks: 1,
+    jenis: 'PRAKTIKUM',
+  },
+]
